ec8cb005a31e51d5163f1439e23a20aef86e5224
crzorz <sokono.person@gmail.com>
2018-07-15 04:54:00 +0900

    modify

--- ahoge/026-salad/main.js
+++ ahoge/026-salad/main.js
@@ -92,9 +92,9 @@ var main = function () {
       new Timeline(salt).show();
 
       if (e.x >= wrist.x+wristRect.left && e.y >= wrist.y+wristRect.top && e.x <= wrist.x+wristRect.right && e.y <= wrist.y+wristRect.bottom) {
-        var voice = game.assets['death-cry.wav'].clone();
-        voice.volume = 1;
-        voice.play();
+        var voiced = game.assets['death-cry.wav'].clone();
+        voiced.volume = 1;
+        voiced.play();
 
         var wristLine = new Timeline(wrist);
         wristLine.moveBy(-25, -2, 2);
